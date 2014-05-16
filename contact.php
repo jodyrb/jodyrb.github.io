45cf1ab3a2d5e2e2fe7bae5c4f7b547a4aac6ce1
jodyrb <jody.burgess@gmail.com>
2014-05-15 22:14:48 -0400

    405
    trying to fix 405 error

--- contact.php
+++ contact.php
@@ -1,6 +1,9 @@
 
 <?php
-@HeaderParam("content-type") String content-type=application/json
+if ($request_method != GET) {
+  proxy_pass http://foobar;
+  break;
+}
 if (isset($_POST['name'], $_POST['mobile'])) {
 	echo 'Your name is ' . $_POST['name'];
 }
