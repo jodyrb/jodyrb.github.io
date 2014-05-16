b953f01c61a2157245541f2e6704956ad82c3065
jodyrb <jody.burgess@gmail.com>
2014-05-16 10:27:29 -0400

    405
    still trying to fix 405

--- process.php
+++ process.php
@@ -1,25 +1,5 @@
 <?php
-server {
-    listen 80;
-    client_max_body_size 4G;
- 
-    access_log /var/sites/webapp/logs/access.maintenance.log;
-    error_log /var/sites/webapp/logs/error.maintenance.log info;
- 
-    server_name api.webapp.com;
- 
-    # this guy redirects any path to /api.json
-    rewrite ^.*$ /api.json last;
- 
-    location / {
-        root /var/sites/webapp/webapp/;
-        index api.json /api.json;
- 
-        # this is the magic
-        error_page 405 = $uri;
-    }
- 
-}
+
 $errors         = array();  	// array to hold validation errors
 $data 			= array(); 		// array to pass back data
 
